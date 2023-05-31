d46d3cdaea9d587af03b244236e195998405fe45
ohmissam <massimomerla.94@gmail.com>
2023-05-31 17:41:29 +0200

    Modified the sigmoid to be the approx sigmoid

--- Sigmoid.m
+++ Sigmoid.m
@@ -9,5 +9,11 @@
 %       y   = Sigmoid of x with same input size
 
 function [y] = Sigmoid(x)
-    y = 1./(1+exp(-x));
+    %y = 1./(1+exp(-x));
+    
+    sz = size(x);
+    y = reshape(x, [], 1);  % Reshape tensor to a column vector
+    y = y./(abs(y) + 1) + 0.5;  % Apply sigmoid function element-wise
+    y = reshape(y, sz);  % Reshape back to the original tensor size
+    y = y.*0.5;  % Scale down the values
 end
